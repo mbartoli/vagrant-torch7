0b2d56def499899b420cb925e530fe9df507f75d
Michael Bartoli <bartolimichael@gmail.com>
2015-05-29 20:19:30 +0000

    adding torch7 installation

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,1 +1,5 @@
-sudo apt-get -y install python-dev
+sudo apt-get -y install git
+curl -sk https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | bash
+git clone https://github.com/torch/distro.git ~/torch --recursive
+cd ~/torch; ./install.sh
+source ~/.bashrc
